2185e9d4313e562cf99e2c0b02c4fc48de9dfc79
Dwayne1205 <157167588+Dwayne1205@users.noreply.github.com>
2024-02-10 16:51:57 -0600

    Create JuegoPrueba.h
    Prueba para ver si fue por una mala agregacion de archivos

---
+++ JuegoPrueba.h
@@ -0,0 +1,28 @@
+#ifndef JUEGOPRUEBA_H
+#define JUEGOPRUEBA_H
+
+#include "Mazo.h"
+#include "JugadorGenerico.h"
+#include "Lista.h"
+#include"Nodo.h"
+
+class JuegoP {
+
+public:
+
+	Juego();
+	~Juego();
+	void Jugadores(JugadorGenerico*);
+	void SacarJugador(Nodo*);
+	void Dealer();
+	Mazo* getBaraja();
+	Lista* getListaJugadores();
+
+private:
+
+	Mazo* Baraja;
+
+	Lista* listaJugadores;
+
+};
+#endif
